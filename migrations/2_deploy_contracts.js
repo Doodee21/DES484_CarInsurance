aeda00c936af6bec255cfc293718bfaa8910b866
Doodee21 <6422780187@g.siit.tu.ac.th>
2024-12-07 21:08:18 +0700

    Claim update try deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,16 @@
-const CarInsuranceClaimProcessing = artifacts.require("CarInsuranceClaimProcessing");
-const CarInsurancePayoutDistribution = artifacts.require("CarInsurancePayoutDistribution");
+const RoleManagement = artifacts.require("RoleManagement");
+const PolicyManagement = artifacts.require("PolicyManagement");
+const CarInsuranceClaimSystem = artifacts.require("CarInsuranceClaimSystem");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(CarInsuranceClaimProcessing);
-    const claimProcessingInstance = await CarInsuranceClaimProcessing.deployed();
-    
-    await deployer.deploy(CarInsurancePayoutDistribution, claimProcessingInstance.address);
+  // Deploy RoleManagement and PolicyManagement first
+  await deployer.deploy(RoleManagement);
+  await deployer.deploy(PolicyManagement);
+
+  // Get the deployed contract addresses
+  const roleManagementAddress = RoleManagement.address;
+  const policyManagementAddress = PolicyManagement.address;
+
+  // Deploy CarInsuranceClaimSystem with RoleManagement and PolicyManagement addresses
+  await deployer.deploy(CarInsuranceClaimSystem, roleManagementAddress, policyManagementAddress);
 };
