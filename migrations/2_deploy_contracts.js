c0cae604f124b3b2c8f38cda8d107138d049d357
Doodee21 <6422780187@g.siit.tu.ac.th>
2024-12-12 14:08:47 +0700

    arai mai roo

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,9 @@ const PolicyManagement = artifacts.require("PolicyManagement");
 const CarInsuranceClaimSystem = artifacts.require("CarInsuranceClaimSystem");
 
 module.exports = async function (deployer) {
-  // Deploy RoleManagement and PolicyManagement first
-  await deployer.deploy(RoleManagement);
-  await deployer.deploy(PolicyManagement);
+  // Deploy RoleManagement with an array containing the single address
+  await deployer.deploy(RoleManagement, ["0x6571C20AF21D33cbcfE5d9d24f175636069bBA74"]);  // Pass the address in an array
+  await deployer.deploy(PolicyManagement, ["0x6571C20AF21D33cbcfE5d9d24f175636069bBA74"]);
 
   // Get the deployed contract addresses
   const roleManagementAddress = RoleManagement.address;
